0c4fd2a716ef71f793a6d7e6a4e72929f15d3d60
nick <nicholasdario11@gmail.com>
2023-03-28 12:03:55 -0700

    more cleanup

--- src/pages/stream/socket-prototype.tsx
+++ src/pages/stream/socket-prototype.tsx
@@ -13,15 +13,14 @@ export default () => {
     const [flipped, setFlipped] = useState(true)
     const [videoEnabled, setVideoEnabled] = useState(true)
     const [audioEnabled, setAudioEnabled] = useState(true)
-    
 
     return <div id="mirror-root" style={{
-        display: "flex",    //  flex-box is excellent for arranging pages. usually best when nested: a parent flex element to define boundery, child to be oriented within parent, then content within that. This allows content to be swapped out easily and the overall position on the page to be relatively stable. 
+        display: "flex",
         
     }} >
 
         <div id="fullScreenVideoContainer" style={{
-            position: "absolute",   //  position absolute is very useful and powerful, but generally should be avoided because it is not "responsive" which in web design terms means capable of displaying more or less correctly no matter what the device is. flexboxes on the other hand are a highly responsive technique that allows rules to be defined around the display of child elements, such that a horizontal list smoothly stacks its content as the window narrows. This is not always desirable, but is a reasonable default behavior.
+            position: "absolute", 
             left:"0px",
             right:"0px"
         }}>
