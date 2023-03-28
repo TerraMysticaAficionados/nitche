c7223f8f1455733794efb66ab32136cbbcf13add
nick <nicholasdario11@gmail.com>
2023-03-28 15:29:27 -0700

    add shaka player component

---
+++ src/lib/components/ShakaPlayer.tsx
@@ -0,0 +1,39 @@
+import { useEffect, useRef } from "react";
+import shaka from 'shaka-player/dist/shaka-player.ui.js'
+
+const STREAM_ID = "socket_prototype_test"
+const manifestUri = "http://localhost:8080/recordings/" + STREAM_ID + "/manifest.mpd"
+
+// shaka player docs
+//  https://shaka-player-demo.appspot.com/docs/api/tutorial-basic-usage.html
+export default function Player() {
+    const videoContainerRef = useRef<HTMLDivElement>(null)
+    const videoRef = useRef<HTMLVideoElement>(null)
+
+    useEffect(() => {
+        if(!videoRef.current || !videoContainerRef.current) return
+        var player = new shaka.Player(videoRef.current);
+        player.addEventListener('error', (error:any) => {
+            console.log(error)
+        });
+        player.load(manifestUri);
+        const ui = new shaka.ui.Overlay(player, videoContainerRef.current, videoRef.current);
+        const controls = ui.getControls();
+        // const ui = new shaka.ui.Overlay(player, videoContainer, video);
+        // const controls = ui.getControls();
+        
+        player.configure({});
+        return () => {
+            
+        }
+    }, [])
+
+    return <>
+        <div ref={videoContainerRef} style={{
+            "display":"flex",
+            "width": "800px"
+        }}>
+        <video id="video" ref={videoRef} controls={true} autoPlay={true}></video>
+        </div>
+    </>
+}
