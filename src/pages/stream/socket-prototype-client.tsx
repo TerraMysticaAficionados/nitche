fe784a43b12cc076d2cf3c141bc6f09d89ab68e3
nick <nicholasdario11@gmail.com>
2023-03-28 15:29:12 -0700

    rename player component

--- src/pages/stream/socket-prototype-client.tsx
+++ src/pages/stream/socket-prototype-client.tsx
@@ -1,2 +1,2 @@
 import dynamic from "next/dynamic"
-export default dynamic(import ("../../lib/components/player"),{ssr:false});
+export default dynamic(import ("../../lib/components/ShakaPlayer"),{ssr:false});
