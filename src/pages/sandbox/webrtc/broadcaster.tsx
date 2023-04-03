938cd5a57042e748442027edbcc4eee1bbb4e55d
nick <nicholasdario11@gmail.com>
2023-04-02 18:32:40 -0700

    cleanup

--- src/pages/sandbox/webrtc/broadcaster.tsx
+++ src/pages/sandbox/webrtc/broadcaster.tsx
@@ -1,6 +1,5 @@
 import { useEffect, useRef, useState } from "react";
 import useWindowSize from "@/lib/hooks/useWindowSize";
-import WebSocketStream from "websocket-stream";
 import ConnectionClient from "@/lib/common/ClientConnection";
 import { WebRTCRecorder } from "@/lib/components/WebRTCRecorder";
 
@@ -45,71 +44,4 @@ export default () => {
         </div>
     </div>
 }
-
-function WebRTCPlayer({style, audio, video}:any) {
-    const videoRef = useRef<HTMLVideoElement>(null)
-    const [localStream] = useMedia(videoRef, audio, video)
-    return <video ref={videoRef} autoPlay={true} style={style} controls={true} muted={true}></video>
-}
-
-function useMedia(videoRef:any, audio:boolean, video:boolean) { 
-    const localPeerConnection = useRef<RTCPeerConnection|null>(null)
-    let stream = useRef<MediaStream|null>(null)
-    const constraints = {
-        video,
-        audio,
-    };
-
-    useEffect(() => {
-        stream?.current?.getAudioTracks().forEach(track => {
-            console.log("disabled audio")
-            track.enabled = audio
-        })
-        stream?.current?.getVideoTracks().forEach(track => {
-            console.log("disabled video")
-            track.enabled = video
-        })
-    }, [audio, video])
-
-    useEffect(() => {
-        if(navigator.mediaDevices.getUserMedia == undefined) {
-            alert('Your browser does not support getUserMedia API');
-            return
-        } else if (videoRef?.current == undefined) {
-            return
-        }
-        const videoElem = videoRef?.current
-        let canceled = false
-        navigator.mediaDevices.getUserMedia(constraints).then((localMediaStream) => {
-            if(canceled) {
-                return
-            }
-            const connectionClient = new ConnectionClient({
-                host: "http://localhost:8080/",
-                prefix: "webrtc-broadcaster"
-            })
-            videoElem.srcObject = localMediaStream
-            stream.current = localMediaStream
-            return connectionClient.createConnection({
-                beforeAnswer: (peerConnection:RTCPeerConnection) => {
-                    localMediaStream.getTracks().forEach(track => {
-                        peerConnection.addTrack(track, localMediaStream)
-                    })
-                }
-            })
-        }).then((peerConnection) => {
-            localPeerConnection.current = peerConnection || null
-        })
-
-        return () => {
-            localPeerConnection?.current?.close()
-            canceled = true
-            videoElem.srcObject = null
-        }
-    }, [videoRef])
-
-    return [stream]
-}
-
-
 
